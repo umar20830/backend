c20d8dc4343b3c6c10e4e5155360f97204e286c7
10Techies <10techies@gmail.com>
2024-08-28 18:32:00 +0500

    Status Code

---
+++ 10-status-code/app.js
@@ -0,0 +1,34 @@
+/*
+
+Status Code:
+
+
+100 : Jab server request accept kr lei.
+
+101 : Jab server apki request ko redirect ke dei kisi dusra protocol par apki condition ka base par.
+
+200 : OK status All work done perfectly.
+
+201 : Created status Server par kuch new create howa hai.
+
+202 : Server na request accept kr lei hai lakin abhi us par working nai shuru ki ya ek asynchronous request hoti hai kuyn ka is request ki complete hona ka koi idea nai hota.
+
+203 : Jab apki request ko ek server sa dusra server par snd kiya jai ya 2 tareeqo sa hota hai phla {distributive server} jo ka ek hi company ka hota hain or dusra {third party server}.
+
+301 : Jab apki request ka url perament change kr diya gya ho.
+
+307 : Jab apki request ka url ko temporary redirect kr diya gya ho.
+
+308 : Jab apki request ka url ko permanent redirect kr diya gya ho.
+
+400 : Bad request jab apko koi bad request server par bhejain.
+
+401 : Jab ap kisi action ka liya authorized na hon like delete or edit something.First you {authenticate} yourself than do this things.
+
+403 : Jab ap koi aisi request karain server sa jis ka liya ap authorized na hon like kisi or user ki details maangna etc. 
+
+404 : Not Found
+
+
+*/
+
