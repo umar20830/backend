3f021fcae18fc16d557693f1c9bbf47037bb6bb4
10Techies <10techies@gmail.com>
2024-08-28 17:14:03 +0500

    HTTP

---
+++ 09-http-methods/app.js
@@ -0,0 +1,17 @@
+/* 
+
+Kuch basics requests jo hum {http} ma use krta hain.
+
+
+01. GET : Jab server sa kuch mangwaana ho
+
+02. POST : Jab server par kuch save krna ho ya kuch create krna ho
+
+03. Patch : Jab server sa kuch particular cheez ko update krna ho
+
+04. Put : Patch ki tarah hi kam krta hai lakin saray data ko update/rewrite kr deta hai chahai kuch thora hi update kuyn na krna ho
+
+05. Delete : Jaisa ka iska name sa zahir hai ya server sa kuch delete krna ka liya use hota hai
+
+
+*/
